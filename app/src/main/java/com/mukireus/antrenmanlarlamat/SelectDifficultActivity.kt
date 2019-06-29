40922637680377528247ccc3d3d8628981247a7d
mukireus <muhammet.omer@outlook.com>
2019-06-29 03:06:28 +0300

    modified main activity design

--- app/src/main/java/com/mukireus/antrenmanlarlamat/SelectDifficultActivity.kt
+++ app/src/main/java/com/mukireus/antrenmanlarlamat/SelectDifficultActivity.kt
@@ -1,12 +1,20 @@
 package com.mukireus.antrenmanlarlamat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_select_difficult.*
 
 class SelectDifficultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_difficult)
-    }
+
+        btnEasy.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            // start your next activity
+            startActivity(intent)
+        }    }
 }
